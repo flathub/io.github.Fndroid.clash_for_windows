3e5926994b36ebcc5421ba3b931f0f7e13a3eecc
tzfljuv <36599326+tzfljuv@users.noreply.github.com>
2022-10-22 07:49:42 +0000

    Update and rename cfw_term.sh to cfw.sh

--- cfw.sh
+++ cfw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-migrate_checker.sh
+gtk-launch migrate_checker.sh || 0
 export TMPDIR=$XDG_RUNTIME_DIR/app/$FLATPAK_ID
 exec zypak-wrapper /app/extra/clash_for_windows/cfw "$@" || 0
 pkill clash-linux
