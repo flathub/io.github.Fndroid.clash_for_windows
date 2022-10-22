0063b76ce8844030e786287178c972ea352732a8
tzfljuv <36599326+tzfljuv@users.noreply.github.com>
2022-10-22 08:08:38 +0000

    Update cfw.sh

--- cfw.sh
+++ cfw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-gtk-launch /app/bin/migrate_checker.sh || true
+gtk-launch /app/extra/migrate_checker.desktop || true
 export TMPDIR=$XDG_RUNTIME_DIR/app/$FLATPAK_ID
 zypak-wrapper /app/extra/clash_for_windows/cfw "$@" || true
 pkill clash-linux
